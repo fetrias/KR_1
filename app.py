efd37e75322acf43980724135b14832da341585c
fetrias <fetrias7@gmail.com>
2026-02-25 23:54:21 +0300

    Task 1.4 completed

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
+from models import User
 
 app = FastAPI()
 
 class Numbers(BaseModel):
-    num1: float  
+    num1: float
     num2: float
+
+my_user = User(
+    name="Петушкова Дарья",
+    id=1
+)
 
 @app.get("/")
 async def root():
@@ -16,3 +22,7 @@ async def root():
 async def calculate(numbers: Numbers):
     result = numbers.num1 + numbers.num2
     return {"result": result}
+
+@app.get("/users")
+async def get_user():
+    return my_user

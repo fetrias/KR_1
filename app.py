1db85de5340cd648c9750ebb2d283d16966283be
fetrias <fetrias7@gmail.com>
2026-02-25 23:48:40 +0300

    Task 1.3 completed

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Numbers(BaseModel):
+    num1: float  
+    num2: float
 
 @app.get("/")
 async def root():
     return FileResponse("index.html")
+
+@app.post("/calculate")
+async def calculate(numbers: Numbers):
+    result = numbers.num1 + numbers.num2
+    return {"result": result}
